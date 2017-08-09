e9fae0192cdd24ed84aaa3e9108a9aa309730092
quiggyb <avessey@lsa2.com>
2017-08-09 15:41:27 -0400

    Added templated slowsort implementation.

---
+++ sort/slowsort.h
@@ -0,0 +1,39 @@
+namespace qlib {
+namespace sort {
+
+template <class RandomIt>
+void slowsort(RandomIt first, RandomIt last) {
+    if (first >= last) {
+        return;
+    }
+
+    RandomIt mid = first + (std::distance(first, last) / 2);
+    qlib::sort::slowsort(first, mid);
+    qlib::sort::slowsort(mid + 1, last);
+
+    if (*last < *mid) {
+        std::iter_swap(mid, last);
+    }
+
+    qlib::sort::slowsort(first, last - 1);
+}
+
+template <class RandomIt, class Compare>
+void slowsort(RandomIt first, RandomIt last, Compare comp) {
+    if (first >= last) {
+        return;
+    }
+
+    RandomIt mid = first + (std::distance(first, last) / 2);
+    qlib::sort::slowsort(first, mid);
+    qlib::sort::slowsort(mid + 1, last);
+
+    if (comp(*last, *mid)) {
+        std::iter_swap(mid, last);
+    }
+
+    qlib::sort::slowsort(first, last - 1);
+}
+
+}
+}
